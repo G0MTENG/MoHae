91b33272e8424c544648b0b2ba21836d7a41a6d5
g0mteng <gomteng03@naver.com>
2024-12-11 21:54:37 +0900

    fix: styled components boolean props 에러 해결

--- frontend/src/components/modules/NavBlock.tsx
+++ frontend/src/components/modules/NavBlock.tsx
@@ -4,7 +4,7 @@ import { NavLink, NavLinkProps, useLocation } from 'react-router-dom'
 import { IconType } from '@/types'
 import { COLORS } from '@/constants'
 
-const Block = styled(NavLink)<{ active: boolean }>`
+const Block = styled(NavLink)<{ $active: boolean }>`
   width: 100%;
   height: 44px;
   display: flex;
@@ -13,7 +13,7 @@ const Block = styled(NavLink)<{ active: boolean }>`
   align-items: center;
   gap: 4px;
 
-  color: ${(props) => (props.active ? COLORS.BLACK500 : COLORS.GRAY500)};
+  color: ${(props) => (props.$active ? COLORS.BLACK500 : COLORS.GRAY500)};
   text-decoration: none;
 `
 
@@ -27,7 +27,7 @@ export const NavBlock = ({ icon, title, ...props }: NavBlockProps) => {
   const Icon = Icons[icon]
 
   return (
-    <Block active={locaion.pathname === props.to} {...props}>
+    <Block $active={locaion.pathname === props.to} {...props}>
       <Icon size={24} />
       <BMJua.Caption>{title}</BMJua.Caption>
     </Block>
